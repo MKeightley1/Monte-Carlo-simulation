04e05a4e29b97dba9a0424ec79430d9734ff19b6
Maurice Keightley <maurice.keightley@gmail.com>
2017-11-14 12:53:29 +1000

    Add files via upload
    With given input parameters - Average PI is provided.

--- estimatedPI.js
+++ estimatedPI.js
@@ -1,16 +1,35 @@
 
 //gets an array - checks if in circle area
 exports.getPointsWithinCircle = function ( gridsize , plot_array, circle_diameter) {
+
 	plots_in_circle=0;
 	plots_out_circle=0;
 	grid_middle=0
 	grid_middle = gridsize/2;
 	
+	for(i=0;i<plot_array.length;i++){
+		x_distance = grid_middle-plot_array[i][0];
+		y_distance = grid_middle-plot_array[i][1];
+		
+		//ensure all distances from grid center is > 0
+		if(x_distance<0){
+			x_distance= Math.abs(x_distance);
+		}
+		if(y_distance<0){
+			y_distance= Math.abs(y_distance);
+		}
+		
+		grid_distance = Math.sqrt(pow(x_distance,2)+Math.pow(y_distance,2));
+		
+		//check how many are in circle
+		if(grid_distance<=(circleDiameter/2)){
+			plots_in_circle++;
+		}
+		
+	}
 	
 	
-	
-	
-    return 1;
+    return plots_in_circle;
 };
 
 
@@ -20,11 +39,19 @@ exports.getPointsWithinCircle = function ( gridsize , plot_array, circle_diamete
 
 
 //create an array of 100 points on the grid
-exports.generateGridPoints = function (gridsize, n) {
+exports.getInnerCirclePoints = function (gridsize, circleDiameter , n) {
 	x=0;
 	y=0;
-	plotPoint_array = [];
-	while(plotPoint_array.length<n){
+	plots_in_circle=0;
+	plots_out_circle=0;
+	grid_middle=0
+	grid_middle = gridsize/2;
+	
+	//arrays 
+	x_plotPoint_array = [];
+	y_plotPoint_array = [];
+	
+	while(x_plotPoint_array.length<n){
 		
 		//plot status to determine if plot points are already on grid
 		plot_status=false;
@@ -33,20 +60,39 @@ exports.generateGridPoints = function (gridsize, n) {
 		x = Math.floor((Math.random() * gridsize) + 1);
 		y = Math.floor((Math.random() * gridsize) + 1);
 		
-		//check array if plots already excist
-		plotPoint_array.forEach(function (plot) {
-		  if(plot[0]==x&&plot[1]==y){
-			  plot_status=true;
-		  }
-		})
-		//if plot not on grid - add to array of plots
-		plot_array =[];
-		plot_array[0]=x;
-		plot_array[1]=y;
+		//use x lenght as an array index
+		for(i=0;i<x_plotPoint_array.length;i++){
+			if(x_plotPoint_array[i]==x&&y_plotPoint_array[i]==y){
+				plot_status=true;
+			}
+		}
+		
+		//if not found - add x and y to list
 		if(!plot_status){
-			plotPoint_array[plotPoint_array.length+1]=plot_array;
+			x_plotPoint_array.push(x);	
+			y_plotPoint_array.push(y);	
+			
+			
+			x_distance = grid_middle-x;
+			y_distance = grid_middle-y;
+		
+			//ensure all distances from grid center is > 0
+			if(x_distance<0){
+				x_distance= Math.abs(x_distance);
+			}
+			if(y_distance<0){
+				y_distance= Math.abs(y_distance);
+			}
+			
+			//use maths to calculate distance
+			grid_distance = Math.sqrt(Math.pow(x_distance,2)+Math.pow(y_distance,2));
+			
+			//check how many are in circle
+			if(grid_distance<=(circleDiameter/2)){
+				plots_in_circle++;
+			}
 		}
 	}
 
-    return plotPoint_array.length;
+    return plots_in_circle;
 };
--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 var http = require('http');
-var pi = require('./estimatedPI');
+
 
 http.createServer(function (req, res) {
+	var estimatepi = require('./estimatedPI');
 	
-	//get plot array
-	grid_points = [];
-	grid_points = pi.generateGridPoints(1000,100);
+	//input parameters
+	grid_dimensions = 1000;
+	circleDiameter=900;
+	n= 1000;
+	iterations =100;
 	
-	//get tally of grid_points within circle
-	circle_points = pi.getPointsWithinCircle(1000,grid_points,900);	
 	
+	total_pi=0;
+	for(i=0;i<iterations;i++){
+		//get tally of grid_points within circle
+		inner_circle_points = estimatepi.getInnerCirclePoints(grid_dimensions,circleDiameter,n);
+				
+		//get percentage 
+		plotsWithinCirclePercentage = inner_circle_points/n;
+				
+		//calculate area of circle
+		estimated_circle_area = plotsWithinCirclePercentage*Math.pow(grid_dimensions,2);
+		
+		//calculate radius
+		radius= (circleDiameter/2);
+		pi=estimated_circle_area/(Math.pow(radius,2));
+		
+		//tally the pis
+		total_pi+= pi;
+	}
 	
+			
+			
+			
+			
+
     res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write("The estimated value of PI is: " + grid_points);
+	res.write("The estimated value of PI is: " + total_pi);
     res.end();
 	
 }).listen(8080);
